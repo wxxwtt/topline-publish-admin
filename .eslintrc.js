ab852d4263c4bfea2209de97cf8d18233d884c86
lipengzhou <lipengzhou@itcast.cn>
2019-06-21 14:58:08 +0800

    修改了代码风格配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,10 @@ module.exports = {
   //   "error" or 2 - 将规则视为一个错误 (退出码为1)，报错并退出
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     // 'semi': 'off',
     // 'semi': ['error', 'always'],
-    "space-before-function-paren": ["error", "never"]
+    // "space-before-function-paren": ["error", "never"]
   },
   parserOptions: {
     parser: 'babel-eslint'
