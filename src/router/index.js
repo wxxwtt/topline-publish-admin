c147281c5d99782deac8c07b91cc467b35a12a51
lipengzhou <lipengzhou@itcast.cn>
2019-06-23 18:52:49 +0800

    使用路由拦截器处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // {
     //   // 路由的名字，和组件名没关系，说白了就是 path 的别名
@@ -16,7 +16,7 @@ export default new Router({
     //   component: () => import('@/views/home')
     // },
     {
-      name: 'layout',
+      // name: 'layout', // 由于它有默认子路由，所以它的名字没有意义，否则 Vue 会给你发黄牌警告
       path: '/',
       component: () => import('@/views/layout'),
       // 嵌套路由：https://router.vuejs.org/zh/guide/essentials/nested-routes.html
@@ -41,3 +41,35 @@ export default new Router({
     }
   ]
 })
+
+/**
+ * 所有路由导航都要经过这里
+ * to 去哪儿
+ * from 来自哪里
+ * next 允许通过的方法
+ */
+
+router.beforeEach((to, from, next) => {
+  const userInfo = window.localStorage.getItem('user_info')
+
+  // 如果是非 /login 页面，判断其登录状态
+  if (to.path !== '/login') {
+    // 如果没有登录，让其跳转到登录页
+    if (!userInfo) {
+      next({ name: 'login' })
+    } else {
+      // 如果登录了，则允许通过
+      next()
+    }
+  } else {
+    // 如果登录了，就不允许访问登录页了
+    if (userInfo) {
+      next(false)
+    } else {
+      // 没有登录，才允许访问登录页
+      next()
+    }
+  }
+})
+
+export default router
