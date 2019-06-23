c4ee4581cc0a8d050055ec7448342dbd80382fb2
lipengzhou <lipengzhou@itcast.cn>
2019-06-23 16:51:13 +0800

    Layout组件-创建并配置路由

---
+++ src/views/layout/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <AppHeader/>
+    <AppAside/>
+    <!-- 这里是内容是 Home、发布文章、评论管理。。。。 -->
+  </div>
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
