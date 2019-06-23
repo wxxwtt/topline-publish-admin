d26698aa41f82d6771fa5463651dac805085df4a
lipengzhou <lipengzhou@itcast.cn>
2019-06-23 16:59:10 +0800

    Layout组件-Container布局容器

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <AppHeader/>
-    <AppAside/>
-    <!-- 这里是内容是 Home、发布文章、评论管理。。。。 -->
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <AppAside/>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <AppHeader/>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -23,4 +29,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-aside {
+  background-color: #d0dce7;
+}
+
+.el-header {
+  background-color: #aec0d1;
+}
+
+.el-main {
+  background-color: #e8eff4;
+}
 </style>
