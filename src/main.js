65e413c7db4a16a2f35d40522abca5c4f0ae6910
lipengzhou <lipengzhou@itcast.cn>
2019-06-24 15:37:16 +0800

    配置axios到Vue原型对象

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录，优先加载目录中的 index.js
@@ -10,6 +11,21 @@ import router from './router'
 import './styles/index.less'
 
 import 'nprogress/nprogress.css'
+
+// 配置 axios 的基础路由
+// 也就是说配置了这个东西，你就不用每次都写长长的 http://xxxx
+// 只需要，例如 axios({ url: '/authorizations' })
+// 路径中的 / ，多退少补
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+
+// 几乎每个组件都要使用 axios 去发请求，频繁的 import 就变得非常麻烦
+// 我们可以将一些频繁使用的成员放到 Vue.prototype 中，然后就可以在组件中直接 this.xxx 使用了
+// 为什么？因为所有组件都是 Vue 的实例
+// 所以往 Vue.prototype 中添加的成员可以直接通过组件实例 this 进行访问
+// Vue.prototype.foo = 'bar'
+
+// 往Vue原型对象中添加成员，尽量使用 $名字 起名字，目的是为了防止和组件中的成员冲突
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 
