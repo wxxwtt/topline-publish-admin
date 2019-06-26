6828fdda4abe6c903fe34fc4f01217df6feb1d54
lipengzhou <lipengzhou@itcast.cn>
2019-06-26 18:43:10 +0800

    文章筛选-处理分页页码问题

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -5,7 +5,7 @@
       <div slot="header" class="clearfix">
         <span>筛选条件</span>
       </div>
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form" :model="filterParams" label-width="80px">
         <el-form-item label="状态">
           <el-radio-group v-model="filterParams.status">
             <el-radio label="">全部</el-radio>
@@ -18,6 +18,7 @@
         </el-form-item>
         <el-form-item label="频道">
           <el-select v-model="filterParams.channel_id" placeholder="请选择活动区域">
+            <el-option label="全部" value=""></el-option>
             <el-option
               v-for="item in channels"
               :key="item.id"
@@ -112,6 +113,8 @@
         一：分多少页
           每页多大，默认是10条每页，我们的接口如果没有指定每页条数，则默认也是按照每页10条返回数据
           有多少条数据
+          total  总记录数
+          current-page 当前页码，也就是高亮的那个页码
         二：页面改变加载对应的页码数据
        -->
       <el-pagination
@@ -119,6 +122,7 @@
         layout="prev, pager, next"
         :total="totalCount"
         :disabled="articleLoading"
+        :current-page="page"
         @current-change="handleCurrentChange"
       >
       </el-pagination>
@@ -217,8 +221,8 @@ export default {
     },
 
     onSubmit () {
-      // this.page = 1
-      this.loadArticles()
+      this.page = 1 // 让分页组件的页码回到第1页
+      this.loadArticles() // 加载第1页的数据
     },
 
     handleCurrentChange (page) {
