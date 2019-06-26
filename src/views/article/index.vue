1202e79e22fa0e29c4047dac21a3f91ef60a4e1a
lipengzhou <lipengzhou@itcast.cn>
2019-06-26 17:13:18 +0800

    文章筛选-处理筛选表单字段内容

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,19 +6,26 @@
         <span>筛选条件</span>
       </div>
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="特殊资源">
+        <el-form-item label="状态">
           <el-radio-group v-model="form.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
+            <el-radio
+              v-for="item in statTypes"
+              :key="item.label"
+              :label="item.label"
+            ></el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="活动区域">
+        <el-form-item label="频道">
           <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
+            <el-option
+              v-for="item in channels"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="活动形式">
+        <el-form-item label="时间">
           <el-date-picker
             v-model="form.value1"
             type="daterange"
@@ -155,12 +162,17 @@ export default {
           type: 'danger',
           label: '已删除'
         }
-      ]
+      ],
+      channels: [] // 频道列表
     }
   },
 
   created () {
+    // 加载文章列表
     this.loadArticles()
+
+    // 加载频道列表
+    this.loadChannels()
   },
 
   methods: {
@@ -177,6 +189,15 @@ export default {
         this.articles = data.results // 列表数据
         this.totalCount = data.total_count // 总记录数
         this.articleLoading = false
+      })
+    },
+
+    loadChannels () {
+      this.$http({
+        method: 'GET',
+        url: '/channels'
+      }).then(data => {
+        this.channels = data.channels
       })
     },
 
