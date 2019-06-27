37705ed0df80659264770d7008e0fa8836a7a0db
lipengzhou <lipengzhou@itcast.cn>
2019-06-27 18:25:29 +0800

    用户退出

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -9,7 +9,12 @@
         </span>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>账户设置</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <!--
+            给组件注册原生事件：@原生事件类型.native="处理函数"
+            .native 修饰符：https://cn.vuejs.org/v2/guide/components-custom-events.html#%E5%B0%86%E5%8E%9F%E7%94%9F%E4%BA%8B%E4%BB%B6%E7%BB%91%E5%AE%9A%E5%88%B0%E7%BB%84%E4%BB%B6
+            该事件会被注册给组件的根元素
+           -->
+          <el-dropdown-item @click.native="handleLogout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -26,6 +31,35 @@ export default {
   },
   created () {
     this.userInfo = JSON.parse(window.localStorage.getItem('user_info'))
+  },
+
+  methods: {
+    handleLogout () {
+      console.log(123)
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 清除本地存储中的 用户信息 user_info
+        window.localStorage.removeItem('user_info')
+
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+
+        // 跳转到登录页面
+        this.$router.push({
+          name: 'login'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
   }
 }
 </script>
